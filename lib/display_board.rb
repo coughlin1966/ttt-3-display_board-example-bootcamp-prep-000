b66d20f54bbdb303ae8f172abea0dc7362730225
Peter Coughlin <coughlin1966@gmail.com>
2018-01-23 17:10:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "A Tic Tac Toe Board"
   row1 = "   |   |   "
   row2 = "   |   |   "
   row3 = "   |   |   "
   column1 = "-----------"
   column2 = "-----------"
+  #puts "A Tic Tac Toe Board"
   puts row1
   puts column1
   puts row2
